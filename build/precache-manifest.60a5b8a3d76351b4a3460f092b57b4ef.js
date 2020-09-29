12f6d7bb3d9fe57176e54018cf430eacbe65e9fe
Joseph Meis <jmeis@protonmail.com>
2020-09-29 17:23:43 -0500

    feat(package.json): update dependencies

---
+++ build/precache-manifest.60a5b8a3d76351b4a3460f092b57b4ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c845694dba0a6772edf7329cf7accb47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70afeec1e90f8aacea75",
+    "url": "/static/css/main.a4161034.chunk.css"
+  },
+  {
+    "revision": "27d13f36a938e33e2905",
+    "url": "/static/js/2.3fe27b93.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/static/js/2.3fe27b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70afeec1e90f8aacea75",
+    "url": "/static/js/main.0b766d98.chunk.js"
+  },
+  {
+    "revision": "e2b2dcf7652e7edf011d",
+    "url": "/static/js/runtime-main.6faa723d.js"
+  }
+]);
