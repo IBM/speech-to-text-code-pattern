d2a38aed620587b4ddf22a18eebc1498520b8cc7
Mark-Sturdevant <Mark.Sturdevant@ibm.com>
2021-12-17 15:39:21 -0800

    Update to node 16 CRA 5 and UBI for Docker
    * Docker updated to use UBI
    * Dependencies updated
    * New CRA needed craco to deal with polyfills

--- scripts/experience_test.sh
+++ scripts/experience_test.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Installing Chrome web driver..."
-source <(curl -sSL "${DEVX_SKIT_ASSETS_GIT_URL_RAW:-https://github.com/IBM/devex-skit-assets/raw/v1.2.3}/scripts/install_chrome.sh")
+source <(curl -sSL "${DEVX_SKIT_ASSETS_GIT_URL_RAW:-https://github.com/IBM/devex-skit-assets/raw/2.0.2}/scripts/install_chrome.sh")
 
 echo "Starting Chrome web driver..."
-source <(curl -sSL "${DEVX_SKIT_ASSETS_GIT_URL_RAW:-https://github.com/IBM/devex-skit-assets/raw/v1.2.3}/scripts/start_chrome.sh")
+source <(curl -sSL "${DEVX_SKIT_ASSETS_GIT_URL_RAW:-https://github.com/IBM/devex-skit-assets/raw/2.0.2}/scripts/start_chrome.sh")
 
 echo "Checking for pip"
 pip3 -V
